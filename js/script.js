662570bd84cd0c26ddbcdf084c5e2e60f3d691a0
Gibson <thedevplug@gmail.com>
2018-02-23 18:51:48 -0600

    added new rep

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let projectID = 1
+let projectTitle = 'Gibsons Locations'
+let projectComponents = true
+let projectList = ['title', 'thumb', 'url', 'desc']
+console.log('ID: ' + projectID)
+console.log('Title: ' + projectTitle)
+console.log('Project has all components: ' + projectComponents)
+console.log('List of components: ' + projectList)
---
+++ js/script.two.js
@@ -0,0 +1,19 @@
+let twoArray = [
+  69,
+  'www.thedevplug.com',
+  'TheDevPlug',
+  'This is a website to showcase TheDevPlug.',
+  true,
+  ['number','where','who','what','best']
+]
+let twoObject = {
+  number: 69,
+  where: 'https://example.com',
+  who: 'Project Title',
+  what: 'Project description.',
+  best: true
+  keywords: ['number','where','who','what','best']
+}
+console.log(twoArray[3])
+console.log(twoObject.what)
+console.log(twoObject["what"])
