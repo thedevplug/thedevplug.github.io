31e79cc7561d5b40d678af1c2868e8090c6d0fe0
Gibson <thedevplug@gmail.com>
2018-03-18 21:02:21 -0500

    changed js

--- js/auto.js
+++ js/auto.js
@@ -1,5 +1,5 @@
-function describeAuto (auto) {
-  let about = 'The ' + auto + ' is a German auto.'
+function describeAuto (myauto) {
+  let about = 'The ' + myauto + ' is a German auto.'
   return about
 }
 let myauto = 'BMW'
