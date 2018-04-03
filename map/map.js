c04cf23d98feaec17c7799a0f172eb31d16eb875
Gibson <thedevplug@gmail.com>
2018-04-03 09:44:45 -0500

    map changed

--- map/map.js
+++ map/map.js
@@ -2,20 +2,20 @@ let base = L.map('base').setView([20, -20], 10);
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(base);
 let onemarker = L.marker([40, -40]).addTo(base);
 let onepolygon = L.polygon([
-  [15, 20],
-  [35, 45],
-  [55, 70]
+  [40, 30],
+  [45, 68],
+  [19, 74],
+  [16, 39]
 ]).addTo(base);
-onepolygon.bindPopup('McDonald\'s Farm');
-onemarker.bindPopup('Farm HQ');
-function logPointer (event) {
-  console.log('Point of interest: ' + event.latlng)
-};
-base.on('Click.', logPointer);
+onepolygon.bindPopup('this is the middle east dont go here plz');
+onemarker.bindPopup('water and water and water');
 var basePolyline = [
-    [36, -14],
-    [71, 91],
-    [0, 1]
+  [36, -14],
+  [71, 91],
+  [0, 1]
 ];
 var oneline = L.polyline(basePolyline, {color: 'red'}).addTo(base);
 base.fitBounds(oneline.getBounds());
+base.on('click', function (event) {
+  console.log('here is where u clicked: ' + event.latlng)
+})
