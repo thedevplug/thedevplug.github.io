f4f433003f29b96770b02b7ad012c76813509be1
Gibson <thedevplug@gmail.com>
2018-05-04 02:14:02 -0500

    final project final

--- fortunefive/script.js
+++ fortunefive/script.js
@@ -1,11 +1,11 @@
 let bfinal = L.map('finalm').setView([39.330049, -96.629018], 4);
-let fl1 = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(bfinal);
-let fl2 = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png');
+let fl1 = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png');
+let fl2 = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(bfinal);
 let fl3 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 let finallayer = {
-  'Basic': fl1,
   'Black & White': fl2,
-  'Wild West': fl3
+  'German': fl1,
+  'Pink': fl3
 }
 L.control.layers(finallayer).addTo(bfinal)
 bfinal.on('click', function (event) {
@@ -13,7 +13,7 @@ bfinal.on('click', function (event) {
 })
 function getColor(d) {
     return d === "Male" ? 'Blue' :
-           d === "Female"  ? 'Red' :
+           d === "Female"  ? 'Pink' :
            '#FFEDA0';
 }
 function style (feature) {
@@ -37,5 +37,6 @@ let foptions = {
 L.geoJSON(fortunegeo, foptions).addTo(bfinal)
 function geopopup (feature, layer) {
   let company = feature.properties.COMPANY
-  layer.bindPopup(company)
+  let gender = feature.properties.CEO
+  layer.bindPopup("Company Name: " + company + "</br>" + "CEO Gender: " + gender)
 }
